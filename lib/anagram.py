a3640d17459d5d25ce0ef947dfec721dcc67d3c1
Samwel Maina <abere.maina@gmail.com>
2025-08-13 15:58:01 +0300

    final commit

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
 # your code goes here!
+# lib/anagram.py
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, words):
+        """
+        Return a list of words from 'words' that are anagrams of self.word.
+        """
+        sorted_word = sorted(self.word)
+        return [
+            w for w in words
+            if sorted(w.lower()) == sorted_word and w.lower() != self.word
+        ]
